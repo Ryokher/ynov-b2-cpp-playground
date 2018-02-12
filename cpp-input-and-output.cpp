1846589e2a99ace0c5f70fd64be43163f363edfa
DESKTOP-NBL3DAK\thoma <thomas.mahe@ynov.com>
2018-02-12 14:37:45 +0100

    no message

---
+++ cpp-input-and-output.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>;
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int x,y,z;
+    std::cin>>x>>y>>z;
+    std::cout<<x+y+z;
+    return 0;
+}
